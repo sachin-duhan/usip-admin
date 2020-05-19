877cc90468e62e8d0ca1c995e4d54cbd3178905c
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-19 07:48:03 +0530

    updating notification UI and period controller

--- src/app/component/dashboard/notify/notify.component.ts
+++ src/app/component/dashboard/notify/notify.component.ts
@@ -9,101 +9,102 @@ import { NewApplicationComponent } from '../application/new-application/new-appl
 import { BankDetailsComponent } from '../../dashboard/inters/bank-details/bank-details.component';
 
 @Component({
-  selector: 'app-notify',
-  templateUrl: './notify.component.html',
-  styleUrls: ['./notify.component.css']
+    selector: 'app-notify',
+    templateUrl: './notify.component.html',
+    styleUrls: ['./notify.component.css']
 })
 export class NotifyComponent implements OnInit {
 
-  constructor(private fb: FormBuilder,
-    private _notificationService: NotifyService,
-    private _toast: ToastrService,
-    private sheet: MatBottomSheet,
-    private dialog: MatDialog) { }
+    creating_notification: Boolean = false;
+    constructor(private fb: FormBuilder,
+        private _notificationService: NotifyService,
+        private _toast: ToastrService,
+    ) { }
 
-  public applicationAllow: Boolean = true;
-  public showSettings: Boolean = false;
-  public publicNotification: any = [];
-  public privateNotification: any = [];
-  private loading: Boolean = false;
+    public applicationAllow: Boolean = true;
+    public showSettings: Boolean = false;
+    public publicNotification: any = [];
+    public privateNotification: any = [];
+    private loading: Boolean = false;
 
-  ngOnInit() {
-    this.loading != this.loading;
 
-    // getting the private notification
-    this._notificationService.internNotification().subscribe(res => {
-      this.privateNotification = res.notifications;
-      this.loading != this.loading;
-    }, err => {
-      console.log(err);
-      this.loading != this.loading;
-      this._toast.error(err.error.message, 'No intern notification!');
-    });
+    private fileData: File = null;
+    public input_msg = "Choose a File for Notification";
 
-    // getting the public notification
-    this._notificationService.publicNotification().subscribe(
-      res => {
-        this.publicNotification = res.body;
+    ngOnInit() {
         this.loading != this.loading;
-      }, err => {
-        console.log(err);
-        this.loading != this.loading;
-      }
-    );
-  }
-  public ApplicationStatus(): void {
-    this.applicationAllow = !this.applicationAllow;
-  }
-  public openSettings(): void {
-    this.showSettings = !this.showSettings;
-  }
-  notificationForm = this.fb.group({
-    title: [''],
-    public: ['']
-  });
 
-  submitForm(): void {
-    const data = {
-      title: this.notificationForm.get('title').value,
-      visiblity: this.notificationForm.get('public').value,
-    };
-
-    this._notificationService.postNotification(data).subscribe(body => {
-      this._toast.success('Notification Added', 'Congratulations');
-      this.notificationForm.reset();
-      this.notificationForm.markAsUntouched();
-    },
-      err => {
-        console.log(err);
-        this._toast.error(err.message, 'BAD REQUEST');
-      });
-  }
-
-  updateBank(): void {
-    this.openSettings();
-    this.dialog.open(BankDetailsComponent, {
-      width: '40%',
-      height: '80%'
-    });
-  }
-  updateApplication(): void {
-    this.openSettings();
-    this.sheet.open(NewApplicationComponent);
-  }
-  deleteNotification(data): void {
-    console.log(data);
-    var alert = confirm("Do you want to delete the application!");
-    if (alert) {
-      this._notificationService.deleteNoti(data).subscribe(
-        res => {
-          console.log(res);
-          this._toast.success('Notification deleted successfully', 'Done');
-          document.getElementById(data).classList.add('hide');
+        // getting the private notification
+        this._notificationService.internNotification().subscribe(res => {
+            this.privateNotification = res.notifications;
+            this.loading != this.loading;
         }, err => {
-          console.log(err);
-          this._toast.error(err.message, "BAD REQUEST");
-        }
-      );
+            console.log(err);
+            this.loading != this.loading;
+            this._toast.error(err.error.message, 'No intern notification!');
+        });
+
+        // getting the public notification
+        this._notificationService.publicNotification().subscribe(
+            res => {
+                this.publicNotification = res.body;
+                this.loading != this.loading;
+            }, err => {
+                console.log(err);
+                this.loading != this.loading;
+            }
+        );
     }
-  }
+
+    notificationForm = this.fb.group({
+        title: [''],
+        public: [''],
+        description: ['']
+    });
+
+    submitForm(): void {
+        const data = {
+            title: this.notificationForm.get('title').value,
+            visiblity: this.notificationForm.get('public').value,
+            description: this.notificationForm.get('description').value,
+        };
+        if (!this.fileData) return;
+        let form = new FormData();
+        form.append('title', data.title);
+        form.append('description', data.description);
+        form.append('visiblity', data.description);
+        form.append('image', this.fileData, this.fileData.name);
+
+        console.log(form);
+        return;
+        this._notificationService.postNotification(form).subscribe(
+            res => {
+                this._toast.success(res.message, "success");
+                this.notificationForm.reset();
+                this.notificationForm.markAsUntouched();
+            },
+            err => this._toast.error(err.message, 'Error'));
+    }
+
+    onFileSelected(event) {
+        this.fileData = <File>event.target.files[0];
+        this.input_msg = this.fileData.name;
+    }
+
+    deleteNotification(data): void {
+        console.log(data);
+        var alert = confirm("Do you want to delete the application!");
+        if (alert) {
+            this._notificationService.deleteNoti(data).subscribe(
+                res => {
+                    console.log(res);
+                    this._toast.success('Notification deleted successfully', 'Done');
+                    document.getElementById(data).classList.add('hide');
+                }, err => {
+                    console.log(err);
+                    this._toast.error(err.message, "BAD REQUEST");
+                }
+            );
+        }
+    }
 }
--- src/app/component/dashboard/period-controller/period-controller.component.ts
+++ src/app/component/dashboard/period-controller/period-controller.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { BankDetailsComponent } from '../inters/bank-details/bank-details.component';
+import { MatDialog, MatBottomSheet } from '@angular/material';
+import { NewApplicationComponent } from '../application/new-application/new-application.component';
 
 @Component({
-  selector: 'app-period-controller',
-  templateUrl: './period-controller.component.html',
-  styleUrls: ['./period-controller.component.css']
+    selector: 'app-period-controller',
+    templateUrl: './period-controller.component.html',
+    styleUrls: ['./period-controller.component.css']
 })
 export class PeriodControllerComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private dialog: MatDialog,
+        private sheet: MatBottomSheet,
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    showSettings: Boolean = true;
+    updateBank(): void {
+        this.showSettings = !this.showSettings;
+        this.dialog.open(BankDetailsComponent, {
+            width: '40%',
+            height: '80%'
+        });
+    }
 
+    updateApplication(): void {
+        this.showSettings = !this.showSettings;
+        this.sheet.open(NewApplicationComponent);
+    }
 }
